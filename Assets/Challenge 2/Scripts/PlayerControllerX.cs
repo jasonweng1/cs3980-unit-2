cec4ce43906ef194ad642e2fd8da970d6b9c6d8b
haiweng <hai-weng@uiowa.edu>
2023-09-18 17:13:30 -0500

    initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float cooldown = 0.0f;
+    private float cooldownReset = 0.6f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldown -= Time.deltaTime;
+        Debug.Log(cooldown);
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) & cooldown <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            cooldown = cooldownReset;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+    private float speed = 26.0f;
+    private float xRange = 20.0f;
+    private float upperZRange = 15.0f;
+    private float lowerZRange = 0.0f;
+
+    public GameObject projectilePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -xRange) {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange) {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+
+        if (transform.position.z < lowerZRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, lowerZRange);
+        }
+        if (transform.position.z > upperZRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, upperZRange);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            // Launch a profjectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    }
+}
